61486ff66d17312bf2c839f88eb2ab42025dc9ae
FrancoPanfilo <108556532+FrancoPanfilo@users.noreply.github.com>
2022-09-03 15:10:14 -0300

    Update modal.js

--- modal.js
+++ modal.js
@@ -3,6 +3,7 @@ let servidorUsuarios = JSON.parse(localStorage.getItem("Servidor Usuarios"))
 if (servidorUsuarios == null) {
     servidorUsuarios = new ServidorUsuarios
     servidorUsuarios.usuarios = []
+    let admin = new Usuarios("FrancoPanfilo","")
     localStorage.setItem("Servidor Usuarios", JSON.stringify(servidorUsuarios))
 }
 
