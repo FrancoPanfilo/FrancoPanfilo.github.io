24ac18408865c28fd75bcd64a94362e0f7250ba8
FrancoPanfilo <108556532+FrancoPanfilo@users.noreply.github.com>
2025-06-17 09:19:13 -0300

    a

---
+++ AzaleaGolf/AzaleaMembersCLub/config.js
@@ -0,0 +1,10 @@
+// Configuración de Firebase
+export const firebaseConfig = {
+  apiKey: "AIzaSyCaoKajIMiN3Y8AtPz5X2brHm0YOsFqiuo",
+  authDomain: "azalea-92a39.firebaseapp.com",
+  projectId: "azalea-92a39",
+  storageBucket: "azalea-92a39.appspot.com",
+  messagingSenderId: "564234531814",
+  appId: "1:564234531814:web:ba8f9c434cd576b01e5c27",
+  measurementId: "G-MX1L41XYVE",
+};
